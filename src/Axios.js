3fe1aceaedee793518ec2a2e24124d296d7e80f4
Youssef Abdullah <69092682+Youssef-Abdullahx09@users.noreply.github.com>
2023-05-23 13:35:45 +0300

    Update Axios.js to vercel server

--- src/Axios.js
+++ src/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: " http://localhost:3000" });
+const axiosInstance = axios.create({ baseURL: " https://json-server-vercel-sand-six.vercel.app/" });
 
 export default axiosInstance;
